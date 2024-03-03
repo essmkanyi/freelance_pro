882ee21df3a0fe071b28a1f2cdb7e1b38ecbf5b2
Esther Mkanyika <thecodingess@gmail.com>
2024-03-03 15:47:44 +0300

    Fix line endings

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADSi_wtjTOrlKKKDlPLE1Y12sWSVb-UdA",
+    authDomain: "freelance-pro-686df.firebaseapp.com",
+    projectId: "freelance-pro-686df",
+    storageBucket: "freelance-pro-686df.appspot.com",
+    messagingSenderId: "100225255960",
+    appId: "1:100225255960:web:eea82df0db17dce466116d",
+    measurementId: "G-27SS9T56VY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
